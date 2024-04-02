727e706f005f1ad4f20be686e66e92815895d1cd
PastoraB <serpensfideliocanis@gmail.com>
2024-04-02 11:39:44 -0600

    Ejercicio Basico

---
+++ ejemplo1.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+int main()
+{
+    printf  
+    ("Holla pedazo de verga"); 
+    printf("/MI NOMBRE ES MAXIMO DECIMO MERIDO, GENERAL DE LOS EJERCITOS DEL NOMBRE, PADRE DE UN HIJO ASESINADO, ESPOSO DE UNA MUJER ASESINADA. SERVIDOR DEL AUTENTICO EMPERADOR: MARCO AURELIO");
+
+    return 0;
+}
